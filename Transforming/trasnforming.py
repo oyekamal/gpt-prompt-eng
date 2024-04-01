126ea9d2f80e9b75183b0b4858413dda17fbf2c6
oyekamal <oyekamalkhan@gmail.com>
2024-04-01 14:38:30 +0500

    transforming

---
+++ Transforming/trasnforming.py
@@ -0,0 +1,97 @@
+from openai import OpenAI
+
+import os 
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+from openai import OpenAI
+
+client = OpenAI()
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+  api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message
+  
+# prompt
+
+lamp_review = """
+Needed a nice lamp for my bedroom, and this one had \
+additional storage and not too high of a price point. \
+Got it fast.  The string to our lamp broke during the \
+transit and the company happily sent over a new one. \
+Came within a few days as well. It was easy to put \
+together.  I had a missing part, so I contacted their \
+support and they very quickly got me the missing piece! \
+Lumina seems to me to be a great company that cares \
+about their customers and products!!
+"""
+
+prompt = f"""
+What is the sentiment of the following product review, 
+which is delimited with triple backticks?
+
+Identify the following items from the review text: 
+- Item purchased by reviewer
+- Company that made the item
+
+The review is delimited with triple backticks. \
+Format your response as a JSON object with \
+"Item" and "Brand" as the keys. 
+If the information isn't present, use "unknown" \
+as the value.
+Make your response as short as possible.
+
+Give your answer as a single word, either "positive" \
+or "negative".
+
+Review text: '''{lamp_review}'''
+"""
+# response = get_completion(prompt)
+# print(response.content)
+
+#output
+# positive
+
+# {
+#   "Item": "lamp",
+#   "Brand": "Lumina"
+# }
+
+
+prompt = f"""
+Identify the following items from the review text: 
+- Sentiment (positive or negative)
+- Is the reviewer expressing anger? (true or false)
+- Item purchased by reviewer
+- Company that made the item
+
+The review is delimited with triple backticks. \
+Format your response as a JSON object with \
+"Sentiment", "Anger", "Item" and "Brand" as the keys.
+If the information isn't present, use "unknown" \
+as the value.
+Make your response as short as possible.
+Format the Anger value as a boolean.
+
+Review text: '''{lamp_review}'''
+"""
+response = get_completion(prompt)
+print(response.content)
+
+#output
+# {
+#     "Sentiment": "positive",
+#     "Anger": false,
+#     "Item": "lamp",
+#     "Brand": "Lumina"
+# }
